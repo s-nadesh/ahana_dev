7543f84b70966302200404d6322ce4b3ac836820
s-nadesh <nadesh@arkinfotec.com>
2018-03-08 13:51:28 +0530

    init commit

---
+++ tpl/alerts/alerts.js
@@ -0,0 +1,118 @@
+app.controller('AlertsController', ['$rootScope', '$scope', '$timeout', '$http', '$state', function ($rootScope, $scope, $timeout, $http, $state) {
+
+        //Index Page
+        $scope.loadAlertsList = function () {
+            $scope.isLoading = true;
+            // pagination set up
+            $scope.rowCollection = [];  // base collection
+
+            // Get data's from service
+            $http.get($rootScope.IRISOrgServiceUrl + '/alert')
+                    .success(function (alerts) {
+                        $scope.isLoading = false;
+                        $scope.rowCollection = alerts;
+
+                        //Avoid pagination problem, when come from other pages.
+                        $scope.footable_redraw();
+                    })
+                    .error(function () {
+                        $scope.errorData = "An Error has occured while loading alerts!";
+                    });
+        };
+
+        //For Form
+        $scope.initForm = function () {
+        }
+
+        //Save Both Add & Update Data
+        $scope.saveForm = function (mode) {
+            _that = this;
+
+            $scope.errorData = "";
+            $scope.msg.successMessage = "";
+
+            if (mode == 'add') {
+                post_url = $rootScope.IRISOrgServiceUrl + '/alerts';
+                method = 'POST';
+                succ_msg = 'Alert saved successfully';
+            } else {
+                post_url = $rootScope.IRISOrgServiceUrl + '/alerts/' + _that.data.alert_id;
+                method = 'PUT';
+                succ_msg = 'Alert updated successfully';
+            }
+
+            $scope.loadbar('show');
+            $http({
+                method: method,
+                url: post_url,
+                data: _that.data,
+            }).success(
+                    function (response) {
+                        $scope.loadbar('hide');
+                        $scope.msg.successMessage = succ_msg;
+                        $scope.data = {};
+                        $timeout(function () {
+                            $state.go('configuration.alerts');
+                        }, 1000)
+
+                    }
+            ).error(function (data, status) {
+                $scope.loadbar('hide');
+                if (status == 422)
+                    $scope.errorData = $scope.errorSummary(data);
+                else
+                    $scope.errorData = data.message;
+            });
+        };
+
+        //Get Data for update Form
+        $scope.loadForm = function () {
+            $scope.loadbar('show');
+            _that = this;
+            $scope.errorData = "";
+            $http({
+                url: $rootScope.IRISOrgServiceUrl + "/alerts/" + $state.params.id,
+                method: "GET"
+            }).success(
+                    function (response) {
+                        $scope.loadbar('hide');
+                        $scope.data = response;
+                        $scope.updateState();
+                        $scope.updateAlert();
+                    }
+            ).error(function (data, status) {
+                $scope.loadbar('hide');
+                if (status == 422)
+                    $scope.errorData = $scope.errorSummary(data);
+                else
+                    $scope.errorData = data.message;
+            });
+        };
+
+        //Delete
+        $scope.removeRow = function (row) {
+            var conf = confirm('Are you sure to delete ?');
+            if (conf) {
+                $scope.loadbar('show');
+                var index = $scope.rowCollection.indexOf(row);
+                if (index !== -1) {
+                    $http({
+                        url: $rootScope.IRISOrgServiceUrl + "/alert/remove",
+                        method: "POST",
+                        data: {id: row.alert_id}
+                    }).then(
+                            function (response) {
+                                $scope.loadbar('hide');
+                                if (response.data.success === true) {
+                                    $scope.rowCollection.splice(index, 1);
+                                    $scope.loadAlertsList();
+                                }
+                                else {
+                                    $scope.errorData = response.data.message;
+                                }
+                            }
+                    )
+                }
+            }
+        };
+    }]);
