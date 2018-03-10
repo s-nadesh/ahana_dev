00e0d739b9ebc5a4a438d152e2dce4dc0d6d7c5a
Thangaraj-ark <thangarajp@arkinfotec.com>
2018-03-10 10:56:03 +0530

    Crm - pharmacy database field added

--- js/config.router.js
+++ js/config.router.js
@@ -3199,6 +3199,14 @@ function run($rootScope, $state, $stateParams, $location, $cookieStore, $http, $
         serviceUrl = 'http://medclinic.ark/api/IRISORG/web/v1'
         orgUrl = 'http://medclinic.ark/client';
         clientURL = 'http://medclinic.ark';
+    } else if ($location.host() == 'medizura.ark') {
+        serviceUrl = 'http://medizura.ark/api/IRISORG/web/v1'
+        orgUrl = 'http://medizura.ark/client';
+        clientURL = 'http://medizura.ark';
+    } else if ($location.host() == 'msctrf.ark') {
+        serviceUrl = 'http://msctrf.ark/api/IRISORG/web/v1'
+        orgUrl = 'http://msctrf.ark/client';
+        clientURL = 'http://msctrf.ark';
     } else {
         clientURL = orgUrl = $location.absUrl().split('#')[0].slice(0, -1);
 //        clientURL = orgUrl = $location.protocol() + '://' + $location.host();
